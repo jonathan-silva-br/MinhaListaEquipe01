936d14ef5c4c796a35ce739d1d6a79b6adad117a
unknown <=valdenir.vldo@gmail.com>
2019-11-29 08:40:15 -0300

    trabalhei na lista selecionada

--- app/src/main/java/br/com/equipe01/minhalista/ui/send/SendFragment.java
+++ app/src/main/java/br/com/equipe01/minhalista/ui/send/SendFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -16,20 +19,17 @@ import br.com.equipe01.minhalista.R;
 
 public class SendFragment extends Fragment {
 
-    private SendViewModel sendViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
+
+
         View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        ListView lista = root.findViewById(R.id.lista);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, dados);
+        lista.setAdapter(adapter);
         return root;
     }
+
+    String[] dados = new String[]{"item1", "item2", "item3", "item4", "item5"};
 }
