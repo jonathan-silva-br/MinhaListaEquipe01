4a59b00c2da3a2c68182ed930a832621c528f39d
Matheus Geiser <matheusgeiser@gmail.com>
2019-12-03 10:13:24 -0300

    Alterações e Implementações
    Alterações da MainActivity na área da base lateral para que sempre que for clicado, mude o nome do textView presente na barra lateral. Também alterei o fragmento e a nav_header_main. No fragmento alterei o tipo dos campos de input para EditText, e na nav_header adicionei id aos TextView.
    
    Implementação do uso do SharedPreferences para salvar os dados do email e nome do usuário sem que os mesmos se percam ao reiniciar o aplicativo.

--- app/src/main/java/br/com/equipe01/minhalista/MainActivity.java
+++ app/src/main/java/br/com/equipe01/minhalista/MainActivity.java
@@ -1,12 +1,14 @@
 package br.com.equipe01.minhalista;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,16 +16,21 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    public NavigationView navigationView;
+
+    //Variável que armazenará o nome do usuário
+    public static String nomeUsuario;
+
+    //Variável que armazenará o email
+    public static String emailUsuario;
+
+    //Variáveis que armazenarão os textviews do menu lateral
+    public TextView nomeU;
+    public TextView emailU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +38,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_menu);
+        navigationView = findViewById(R.id.nav_menu);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -51,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        loadData();
     }
 
     @Override
@@ -63,7 +66,67 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+
+        nomeU =  navigationView.findViewById(R.id.nomeUsuario);
+        emailU = navigationView.findViewById(R.id.emailUsuario);
+
+        nomeU.setText(nomeUsuario);
+        emailU.setText(emailUsuario);
+
+        saveData();
+
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    /**
+     *  Método que salva dados do usuário
+     *
+     *
+     * Através de SharedPreferences armazena as informações de nome e email
+     * para que essas não se percam ao reiniciar o aplicativo
+     *
+     *
+     * return void
+     *
+     */
+
+    public void saveData(){
+
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("nome",nomeUsuario);
+        editor.putString("email",emailUsuario);
+
+        editor.apply();
+
+    }
+
+    /**
+     *
+     * Método que carrega os dados salvos do usuário
+     *
+     * Através de SharedPreferences, recupera os dados salvados anteriormente no método saveData()
+     *
+     * return void
+     *
+     */
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE);
+
+        nomeU =  navigationView.findViewById(R.id.nomeUsuario);
+        emailU = navigationView.findViewById(R.id.emailUsuario);
+
+        nomeUsuario = sharedPreferences.getString("nome", "Você");
+        emailUsuario = sharedPreferences.getString("email"," ");
+
+        Log.i("teste",nomeUsuario+" - "+emailUsuario);
+
+    }
+
+
+
 }
--- app/src/main/java/br/com/equipe01/minhalista/ui/tools/ToolsFragment.java
+++ app/src/main/java/br/com/equipe01/minhalista/ui/tools/ToolsFragment.java
@@ -4,32 +4,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import br.com.equipe01.minhalista.MainActivity;
 import br.com.equipe01.minhalista.R;
 
 public class ToolsFragment extends Fragment {
 
-    private ToolsViewModel toolsViewModel;
+    EditText txtNome;
+    EditText txtEmail;
+    Button btnDados;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        toolsViewModel =
-                ViewModelProviders.of(this).get(ToolsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        toolsViewModel.getText().observe(this, new Observer<String>() {
+
+        //Pegando componentes de input do XML
+        txtNome = (EditText)root.findViewById(R.id.txtNome);
+        txtEmail = (EditText)root.findViewById(R.id.txtEmail);
+
+        //Botão de alteração
+        btnDados = root.findViewById(R.id.btnConfirmarInfos);
+
+        TextView lblTexto = root.findViewById(R.id.lblCima);
+
+        if(!MainActivity.nomeUsuario.equals("Você")){
+            btnDados.setText("Alterar Dados");
+
+            lblTexto.setText("Altere seus dados");
+
+            txtNome.setHint(MainActivity.nomeUsuario);
+            txtEmail.setHint(MainActivity.emailUsuario);
+
+            txtNome.setText(MainActivity.nomeUsuario);
+            txtEmail.setText(MainActivity.emailUsuario);
+
+        }
+
+        btnDados.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
+                //Capturando valores dos inputs
+                String nome =  txtNome.getText().toString();
+                String email =  txtEmail.getText().toString();
+
+
+                //Verificação para ver se os valores capturados dos campos são nulos
+                //Se não forem, prossegue com as alterações na navigation view
+                if(nome.equals("") ||  email.equals("")) {
+                    Toast.makeText(getActivity(), "Preencha os campos", Toast.LENGTH_LONG).show();
+                }else{
+                    MainActivity.emailUsuario=email;
+                    MainActivity.nomeUsuario=nome;
+                    Toast.makeText(getActivity(),"Informações alteradas",Toast.LENGTH_LONG).show();
+                    btnDados.setEnabled(false);
+                }
+
             }
         });
+
         return root;
     }
+
+
+
+
 }
